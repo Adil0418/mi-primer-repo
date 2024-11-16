aca55e257ff56d9f89a098a9f138e47fd2c5cb6f
Adi <drakourus@protonmail.com>
2024-11-16 12:43:19 -0600

    class banco crear cliente

--- bbva.java
+++ bbva.java
@@ -14,6 +14,47 @@ class Persona{
     }
 }
 
+class Cliente extends Persona{
+    int numero;
+
+    Cliente(String nombre, String sexo, int edad, int numero){
+        super(nombre, sexo, edad);
+        this.numero = numero;
+    }
+}
+
+class Banco {
+    String nombre;
+    Cliente clientes [];
+
+    Banco (int cantidadClientes, String nombre){
+        clientes = new Cliente[cantidadClientes];
+        this.nombre = nombre;
+    }
+    Cliente creaCliente(String nombre, String sexo, int edad, int numero){
+        System.out.println("Creando cliente...........");
+        Cliente cliente = new Cliente(nombre, sexo, edad, numero);
+
+        for (int i = 0; i < clientes.length; i++){
+            if (clientes [i] == null){
+                clientes [i] = cliente;
+                System.out.println("Cliente creado exitosamente");
+                break;
+            }
+        }
+
+        return cliente;
+}
+
+void listarClientes() {
+    for (int i = 0; i < clientes.length; i++){
+        if (clientes[i] != null){
+            Cliente cliente = clientes[i];
+            System.out.println("Cliente: " + cliente.nombre + "No: " + cliente.numero);
+        }
+    }
+}
+}
 class Programa {
     public static void main(String[] args) {
         System.out.println("Hello world");
